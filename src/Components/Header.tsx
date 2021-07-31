443918c0d7d0dede13cce769a4f4449d7b031f85
Tajiboev <mukhammadjon.tajiboev@gmail.com>
2021-08-01 00:20:46 +0500

    header

---
+++ src/Components/Header.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react";
+import Container from "./Container";
+
+const Header: FC = () => {
+	return (
+		<header>
+			<Container>
+				<h2>Sentiment analysis tool</h2>
+			</Container>
+		</header>
+	);
+};
+
+export default Header;
