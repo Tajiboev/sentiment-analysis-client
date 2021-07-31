7722b2e9a41128800146836f611a94f7950ca41f
Tajiboev <mukhammadjon.tajiboev@gmail.com>
2021-08-01 00:20:40 +0500

    footer

---
+++ src/Components/Footer.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react";
+import Container from "./Container";
+
+const Footer: FC = () => {
+	return (
+		<footer>
+			<Container>
+				<p>This tool was build with educational purpose only!</p>
+			</Container>
+		</footer>
+	);
+};
+
+export default Footer;
