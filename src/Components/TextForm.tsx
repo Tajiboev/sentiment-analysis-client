5af6c32094f7f9f80a6f96ac3e5d066cca4aeb25
Tajiboev <mukhammadjon.tajiboev@gmail.com>
2021-07-31 23:43:20 +0500

    error handling, empty value check

--- src/Components/TextForm.tsx
+++ src/Components/TextForm.tsx
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import React, { Dispatch, useRef, useState } from "react";
 import { FC } from "react";
 import styles from "../Styles/form.module.sass";
@@ -6,14 +6,23 @@ import { Result } from "./Results";
 
 interface Props {
 	setResults: Dispatch<React.SetStateAction<Result[]>>;
+	setError: Dispatch<React.SetStateAction<string>>;
 }
 
-const TextForm: FC<Props> = ({ setResults }) => {
+const TextForm: FC<Props> = ({ setResults, setError }) => {
 	const [loading, setloading] = useState(false);
+	const [inputError, setinputError] = useState(false);
 	const ref = useRef<HTMLTextAreaElement>(null);
 	const handleSubmit = (e: React.FormEvent) => {
 		e.preventDefault();
+		if (!ref.current || ref.current.value === "") {
+			setinputError(true);
+			return;
+		} else {
+			setinputError(false);
+		}
 		setloading(true);
+		setResults([]);
 		if (ref.current) {
 			let text = ref.current.value;
 			axios
@@ -21,14 +30,16 @@ const TextForm: FC<Props> = ({ setResults }) => {
 					text,
 				})
 				.then((result) => {
-					console.log(result);
+					setloading(false);
 					setResults(result.data.data);
-					setloading(false);
 				})
-				.catch((e) => {
+				.catch((e: AxiosError) => {
 					setloading(false);
-					alert("Error fetching data from server");
-					console.error(e);
+					if (e.response) {
+						setError(e.response.statusText);
+					} else {
+						setError("Something went wrong. Please try again!");
+					}
 				});
 		}
 	};
@@ -37,6 +48,7 @@ const TextForm: FC<Props> = ({ setResults }) => {
 			<fieldset className={styles.fieldset}>
 				<legend>Place you text inside this textarea:</legend>
 				<textarea className={styles.textarea} rows={8} ref={ref}></textarea>
+				{inputError && <p className="error">Please enter your text in the textarea!</p>}
 				<button className={styles.button} disabled={loading}>
 					{loading ? "Waiting for results" : "Analyze →"}
 				</button>
